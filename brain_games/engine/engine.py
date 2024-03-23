087109deb8f872609abe05a2f9c0d30cd55272ea
Игорь Рудич <rudi4.clarinet@mail.ru>
2024-03-23 21:56:10 +0300

    add ROUNS_COUNT in engine

--- brain_games/engine/engine.py
+++ brain_games/engine/engine.py
@@ -3,13 +3,13 @@ import prompt
 from brain_games.cli import welcome_user
 
 
-MAX_ROUND_QUANTITY = 3
+ROUNDS_COUNT = 3
 
 
 def start_game(game):
     name = welcome_user()
     print(game.GAME_RULE)
-    for round in range(MAX_ROUND_QUANTITY):
+    for round in range(ROUNDS_COUNT):
         question, correct_answer = game.get_game_data()
         print(f'Question: {question}')
         answer = prompt.string('Your answer: ')
@@ -19,8 +19,6 @@ def start_game(game):
                    f"Let's try again, {name}!"))
             round = 0
             break
-        else:
-            print('Correct!')
+        print('Correct!')
     if round == 2:
         print(f'Congratulations, {name}!')
-
